2b23f3f31571ce63ee0d4714b000e6fbb0dfc227
Christopher Mendoza <tiptop1112@gmail.com>
2025-03-18 16:04:39 -0700

    Created function that creates a fibonacci sequence based on the passed numerical argument if I call fibs(10) then it should return an array the first 10 numbers within the fibonacci sequence

--- fibs.js
+++ fibs.js
@@ -1,1 +1,14 @@
-console.log('Hi')
+function fibs (length) {
+    let array = []
+    let first = 0
+    let second = 1
+    for(i = 0; i < length; i++) {
+        array.push(first) //0
+        let temp = first + second //2
+        first = second //1
+        second= temp //2
+    }
+    return array
+}
+
+console.log(fibs(10))
