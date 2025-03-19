93431146c764f255c3c0806c44f42e6525ad67f8
Christopher Mendoza <tiptop1112@gmail.com>
2025-03-19 13:15:11 -0700

    Created new function fibsRec that uses recursion to solve fibonacci sequence

--- fibs.js
+++ fibs.js
@@ -11,4 +11,22 @@ function fibs (length) {
     return array
 }
 
-console.log(fibs(10))
+
+function fibsRec(length) {
+    if (length === 0) {
+        return [];
+    } else if (length === 1) {
+        return [0];
+    } else if (length === 2) {
+        return [0, 1];
+    }
+    
+    let prevArray = fibsRec(length - 1);
+    let nextFib = prevArray[prevArray.length - 1] + prevArray[prevArray.length - 2];
+    prevArray.push(nextFib);
+    
+    return prevArray;
+}
+
+console.log(fibs(2))
+console.log(fibsRec(5))
